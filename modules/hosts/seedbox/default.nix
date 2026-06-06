c81f5f015ddf20e04ed218c70a5df1b04ad604fb
pepsimax271000 <248238336+pepsimax271000@users.noreply.github.com>
2026-06-06 02:23:59 +0100

    add host

--- modules/hosts/seedbox/default.nix
+++ modules/hosts/seedbox/default.nix
@@ -2,18 +2,14 @@
 {
   flake.nixosConfigurations.seedbox = inputs.nixpkgs.lib.nixosSystem {
     modules = with self.nixosModules; [
-
       shareUser
 
       profileServer
-      nfs
       stylix
       seedboxConfiguration
       seedboxHardware
       seedboxDisko
       homeManager
-
-      inputs.nix-topology.nixosModules.default
       {
         home-manager.users.ye.imports = with self.homeModules; [
           neovim
