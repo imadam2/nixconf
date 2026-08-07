a8cfd42917fffae8dc54b2381dfecb464dc7d753
imadam2 <248238336+imadam2@users.noreply.github.com>
2026-08-07 22:36:43 +0100

    idk

--- modules/base/shell.nix
+++ modules/base/shell.nix
@@ -86,11 +86,11 @@
           settings = {
             "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
 
-            logo = {
-              source = "${config.my.homeDir}/nixconf/assets/pfp2.jpg";
-              type = "sixel";
-              width = 24;
-            };
+            #logo = {
+            #  source = "${config.my.homeDir}/nixconf/assets/pfp2.jpg";
+            #  type = "sixel";
+            #  width = 24;
+            #};
 
             display = {
               separator = "  ";
