3e96dc37e27518e60d2c9b7757f1919736b8fbca
imadam2 <248238336+imadam2@users.noreply.github.com>
2026-08-24 23:24:13 +0100

    try

--- modules/extra/obs.nix
+++ modules/extra/obs.nix
@@ -3,6 +3,36 @@
   flake.homeModules.obs =
     { pkgs, ... }:
     {
+
+      networking.firewall = {
+        allowedUDPPorts = [
+          5353
+          5960
+          5961
+          5962
+          5963
+          6960
+          6961
+          6962
+          6963
+          7960
+          7961
+          7962
+          7963
+        ];
+        allowedTCPPorts = [
+          5959
+          5960
+          6960
+          6961
+          6962
+          6963
+          7960
+          7961
+          7962
+          7963
+        ];
+      };
       programs = {
         obs-studio = {
           enable = true;
